35156b90303e343c005d436398ae091d3b01bc11
neslihan <neslihan_atasvr111@outlook.com>
2023-07-14 23:27:38 +0300

    tailwind css installation and react installation

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'blueColor': '#2a68ff',
+      }
+    },
+  },
+  plugins: [],
+}
+
