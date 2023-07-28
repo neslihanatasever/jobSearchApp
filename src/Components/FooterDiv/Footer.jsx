7bb1445514a8ff55c725534c1afd29a565d42f58
neslihan <neslihan_atasvr111@outlook.com>
2023-07-28 21:50:48 +0300

    fixed footer

--- src/Components/FooterDiv/Footer.jsx
+++ src/Components/FooterDiv/Footer.jsx
@@ -2,7 +2,70 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className='footer p-[5rem] mb-4 bg-blueColor rounded-[10px] gap-8 grid grid-cols-5 m-auto items-center justify-center'>
+      <div>
+      <div className="logoDiv">
+        <h1 className="logo text-[25px] text-white pb-[1.5rem]">
+          <strong>Job</strong>
+          Search</h1>
+      </div>
+      <p className='text-white pb-[13px] opacity-70 leading-7'>
+        We always make our seekers and companies find the best jobs and employers find the best candidates.
+      </p>
+      </div>
+
+      <div className='grid'>
+        <span className='divTitle text-[18px] font-semibold pb-[1.5rem] text-white'>
+          Company
+        </span>
+        <div className='grid gap-3'>
+          
+          <li className='text-white oapcity-[.7] hover:opacity-[1]'>About Us</li>
+          <li className='text-white oapcity-[.7] hover:opacity-[1]'>Features</li>
+          <li className='text-white oapcity-[.7] hover:opacity-[1]'>New</li>
+          <li className='text-white oapcity-[.7] hover:opacity-[1]'>FAQ</li>
+        </div>
+      </div>
+
+      <div className='grid'>
+        <span className='divTitle text-[18px] font-semibold pb-[1.5rem] text-white'>
+          Company
+        </span>
+        <div className='grid gap-3'>
+          
+          <li className='text-white oapcity-[.7] hover:opacity-[1]'>About Us</li>
+          <li className='text-white oapcity-[.7] hover:opacity-[1]'>Features</li>
+          <li className='text-white oapcity-[.7] hover:opacity-[1]'>New</li>
+          <li className='text-white oapcity-[.7] hover:opacity-[1]'>FAQ</li>
+        </div>
+      </div>
+
+      <div className='grid'>
+        <span className='divTitle text-[18px] font-semibold pb-[1.5rem] text-white'>
+          Company
+        </span>
+        <div className='grid gap-3'>
+          
+          <li className='text-white oapcity-[.7] hover:opacity-[1]'>About Us</li>
+          <li className='text-white oapcity-[.7] hover:opacity-[1]'>Features</li>
+          <li className='text-white oapcity-[.7] hover:opacity-[1]'>New</li>
+          <li className='text-white oapcity-[.7] hover:opacity-[1]'>FAQ</li>
+        </div>
+      </div>
+
+      <div className='grid'>
+        <span className='divTitle text-[18px] font-semibold pb-[1.5rem] text-white'>
+          Company
+        </span>
+        <div className='grid gap-3'>
+          
+          <li className='text-white oapcity-[.7] hover:opacity-[1]'>About Us</li>
+          <li className='text-white oapcity-[.7] hover:opacity-[1]'>Features</li>
+          <li className='text-white oapcity-[.7] hover:opacity-[1]'>New</li>
+          <li className='text-white oapcity-[.7] hover:opacity-[1]'>FAQ</li>
+        </div>
+      </div>
+    </div>
   )
 }
 
