03e4251c099d01442ed98e6e17eb785439e6baeb
neslihan <neslihan_atasvr111@outlook.com>
2023-07-28 20:31:29 +0300

    fixed value.jsx section

--- src/Components/JobDiv/Jobs.jsx
+++ src/Components/JobDiv/Jobs.jsx
@@ -1,26 +1,189 @@
-import React from "react";
+import React from 'react';
 
-//Imported Icons ==>
 import { BiTimeFive } from "react-icons/bi";
+import logo1 from "../../Assets/logo.png";
+import logo2 from "../../Assets/logo1.png";
+import logo3 from "../../Assets/logo2.png";
+import logo4 from "../../Assets/logo3.png";
+import logo5 from "../../Assets/logo4.png";
+
+
+import { Component } from "react";
+
+
+const Data = [
+  {
+    id: 1,
+    image: logo1,
+    title: "Web Developer",
+    time: "Now",
+    location: "Canada",
+    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    company: "Netflix"
+  },
+  {
+    id: 2,
+    image: logo2,
+    title: "UI Designer",
+    time: "14Hrs",
+    location: "Manchester",
+    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    company: "Google"
+  },
+  {
+    id: 3,
+    image: logo3,
+    title: "Software Eng.",
+    time: "Now",
+    location: "Austria",
+    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    company: "Instagram"
+  },
+  {
+    id: 3,
+    image: logo4,
+    title: "Frontend Eng.",
+    time: "2Mnths.",
+    location: "Turkey",
+    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    company: "Instagram"
+  },
+  {
+    id: 3,
+    image: logo5,
+    title: "Backend Dev.",
+    time: "12Hrs",
+    location: "South Korea",
+    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    company: "Instagram"
+  },
+  {
+    id: 1,
+    image: logo1,
+    title: "Web Developer",
+    time: "Now",
+    location: "Canada",
+    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    company: "Netflix"
+  },
+  {
+    id: 2,
+    image: logo2,
+    title: "UI Designer",
+    time: "14Hrs",
+    location: "Manchester",
+    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    company: "Google"
+  },
+  {
+    id: 3,
+    image: logo3,
+    title: "Software Eng.",
+    time: "Now",
+    location: "Austria",
+    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    company: "Instagram"
+  },
+  {
+    id: 3,
+    image: logo4,
+    title: "Frontend Eng.",
+    time: "2Mnths.",
+    location: "Turkey",
+    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    company: "Instagram"
+  },
+  {
+    id: 3,
+    image: logo5,
+    title: "Backend Dev.",
+    time: "12Hrs",
+    location: "South Korea",
+    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    company: "Instagram"
+  },
+  {
+    id: 1,
+    image: logo1,
+    title: "Web Developer",
+    time: "Now",
+    location: "Canada",
+    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    company: "Netflix"
+  },
+  {
+    id: 2,
+    image: logo2,
+    title: "UI Designer",
+    time: "14Hrs",
+    location: "Manchester",
+    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    company: "Google"
+  },
+  {
+    id: 3,
+    image: logo3,
+    title: "Software Eng.",
+    time: "Now",
+    location: "Austria",
+    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    company: "Instagram"
+  },
+  {
+    id: 3,
+    image: logo4,
+    title: "Frontend Eng.",
+    time: "2Mnths.",
+    location: "Turkey",
+    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    company: "Instagram"
+  },
+  {
+    id: 3,
+    image: logo5,
+    title: "Backend Dev.",
+    time: "12Hrs",
+    location: "South Korea",
+    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    company: "Instagram"
+  },
+  
+]
 
 const Jobs = () => {
   return (
     <div>
       <div className="jobContainer flex gap-10 justify-center flex-wrap items-center py-10">
-        <div
-          className="group group/item singleJob w-[250px] p-[20px] bg-white rounded [10px] hover:bg-blueColor shadow-lg
+        {
+        Data.map(({ id, image, title, time, location, desc, company }) => {
+          return (
+            <div key={id} className="group group/item singleJob w-[250px] p-[20px] bg-white rounded [10px] hover:bg-blueColor shadow-lg
         shadow-greyIsh-400/70 hover:shadow-lg ">
-          <span className="flex justify-between items-center gap-4">
-            <h1 className="text-[16px] font-semibold text-textColor group-hover:text-white">
-              Web Developer
-            </h1>
 
-            <span className="flex items-center text-[#ccc] gap-1">
-              <BiTimeFive />
-              Now
-            </span>
-          </span>
-        </div>
+              <span className="flex justify-between items-center gap-4">
+                <h1 className="text-[16px] font-semibold text-textColor group-hover:text-white">{title}</h1>
+                <span className="flex items-center text-[#ccc] gap-1"><BiTimeFive />{time}</span>
+              </span>
+
+              <h6 className="text-[#ccc]">{location}</h6>
+              <p className="text-[13px] text-[#959595] pt-[20px] border-t-[2px] mt-[20px] group-hover:text-white">
+                {desc}
+              </p>
+
+              <div className="company flex items-center gap-2">
+                <img src={image} alt="Company Logo" className="w-[10%]" />
+                <span className="text-[14px] py-[1rem] block group-hover:text-white">
+                  {company}
+                </span>
+              </div>
+
+              <button
+                className="border-[2px] rounded-[10px] block p-[10px] w-full text-[14px] font-semibold text-textColor
+           hover:bg-white group-hover/item:text-textColor group-hover:text-white" >Apply Now
+              </button>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
--- src/Components/ValueDiv/Value.jsx
+++ src/Components/ValueDiv/Value.jsx
@@ -1,8 +1,60 @@
 import React from 'react'
 
+import logo7 from "../../Assets/logo6.png";
+import logo8 from "../../Assets/logo7.png";
+import logo9 from "../../Assets/logo8.png";
+
 const Value = () => {
   return (
-    <div>Value</div>
+    <div className='mb-[4rem] mt-[6rem]'>
+      <h1 className='text-textColor text-[20px] py-[2rem] pb-[3rem] font-bold w-[400px] block'>The value that holds us true and to account</h1>
+
+      <div className='grid gap-[10rem] grid-cols-3 items-center'>
+
+        <div className='singleGrid rounded-[10px] hover:bg-[#eeedf7] p-[1.5rem]'>
+          <div className='flex items-center gap-3'>
+            <div className='imgDiv p-[4px] rounded-[.8rem] bg-[#dedef8] h-[40px] w-[40px] flex items-center justify-center'>
+              <img src={logo7} alt='' className='w-[70%]'/>
+            </div>
+            <span className='font-semibold text-textColor text-[18px]'>
+              Simplicity
+            </span>
+          </div>
+          <p className='text-[20px] text-textColor opacity-[.7] py-[1rem] font-semibold'>
+            Things beinf made beautiful simple are at the heart of everything we do.
+          </p>
+        </div>
+
+        <div className='singleGrid rounded-[10px] hover:bg-[#f3e3eee5] p-[1.5rem]'>
+          <div className='flex items-center gap-3'>
+            <div className='imgDiv p-[4px] rounded-[.8rem] bg-[#f7d1e1] h-[40px] w-[40px] flex items-center justify-center'>
+              <img src={logo8} alt='' className='w-[70%]'/>
+            </div>
+            <span className='font-semibold text-textColor text-[18px]'>
+              Simplicity
+            </span>
+          </div>
+          <p className='text-[20px] text-textColor opacity-[.7] py-[1rem] font-semibold'>
+            We believe in making things better for everyone, even if just by a little bit!
+          </p>
+        </div>
+
+        <div className='singleGrid rounded-[10px] hover:bg-[#fcfae3] p-[1.5rem]'>
+          <div className='flex items-center gap-3'>
+            <div className='imgDiv p-[4px] rounded-[.8rem] bg-[#f3f2ad] h-[40px] w-[40px] flex items-center justify-center'>
+              <img src={logo9} alt='' className='w-[70%]'/>
+            </div>
+            <span className='font-semibold text-textColor text-[18px]'>
+              Simplicity
+            </span>
+          </div>
+          <p className='text-[20px] text-textColor opacity-[.7] py-[1rem] font-semibold'>
+            We work on the basis of creating trust which can be nurtured through authenticity and transparency.
+          </p>
+        </div>
+
+      </div>
+    </div>
   )
 }
 
