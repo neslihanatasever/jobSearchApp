3ad9866c083f7830509258a096bb1e3b24841f17
Umut Sirin <umutsirin1@gmail.com>
2023-07-30 16:35:16 -0700

    Update NavBar.jsx

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -1,24 +1,26 @@
 import React from 'react'
 
+const NavItem = ({ children }) => <li className="menuList text-[#6f6f6f] hover:text-blueColor">{children}</li>
+
 const NavBar = () => {
   return (
     <div className='navBar flex justify-between items-center p-[3rem]'>
       <div className="logoDiv">
         <h1 className="logo text-[25px] text-blueColor">
           <strong>Job</strong>
-          Search</h1>
+          Search
+        </h1>
       </div>
 
-      <div className="menu flex gap-8">
-        <li className="menuList text-[#6f6f6f] hover:text-blueColor">Jobs</li>
+      <ul className="menu flex gap-8">
+        <NavItem>Jobs</NavItem>
         <li className="menuList text-[#6f6f6f] hover:text-blueColor">Companies</li>
         <li className="menuList text-[#6f6f6f] hover:text-blueColor">About</li>
         <li className="menuList text-[#6f6f6f] hover:text-blueColor">Contact</li>
         <li className="menuList text-[#6f6f6f] hover:text-blueColor">Blog</li>
         <li className="menuList text-[#6f6f6f] hover:text-blueColor">Login</li>
         <li className="menuList text-[#6f6f6f] hover:text-blueColor">Register</li>
-      </div>
-
+      </ul>
     </div>
   )
 }
